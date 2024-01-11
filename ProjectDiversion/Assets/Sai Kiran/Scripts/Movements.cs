59baaada2ce4c19614ef30c10d1e6393fe2a402a
SaiKiran090 <saikiranj3723@gmail.com>
2024-01-10 16:26:50 -0800

    removed jump for temporary, change capsulcollider while crouch,  lara prefab update and some miner changes

--- ProjectDiversion/Assets/Sai Kiran/Scripts/Movements.cs
+++ ProjectDiversion/Assets/Sai Kiran/Scripts/Movements.cs
@@ -3,13 +3,24 @@ using UnityEngine.InputSystem;
 
 public class Movements : MonoBehaviour
 {
-    private GameInputs inputActions;
     [SerializeField] private Animator animator;
-    private Camera mainCamera;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private CapsuleCollider capsuleCollider;
+    private GameInputs inputActions;
+    private Camera mainCamera;
 
-    private Vector3 playerMove;
+    private Vector3 playerMove_;
     private Vector3 moveDirection;
+    private Vector3 movement;
+    private Vector3 currentPosition;
+    private Vector3 newPosition;
+    private Vector2 intended;
+    private Vector2 currentValue;
+    private Vector2 result;
+    private Vector3 facingDirection;
+    [SerializeField] private Vector3 standCenter;
+    [SerializeField] private Vector3 crouchCenter;
+
 
     private bool playerSprint_;
     private bool playerCrouch_;
@@ -20,11 +31,16 @@ public class Movements : MonoBehaviour
 
     [SerializeField] private float animationTransitionMultiplier = 10f;
     [SerializeField] private float jumpForce = 2f;
-    //private float jumpVelocity;
     [SerializeField] private float walkSpeed;
     [SerializeField] private float sprintSpeed;
-    //[SerializeField] private float gravity = -9.81f;
     [SerializeField] private float rayLength = 0.1f;
+    [SerializeField] private float standHeight;
+    [SerializeField] private float crouchHeight;
+
+    private float applyWalkSpeed;
+    private float applySprintSpeed;
+    //[SerializeField] private float gravity = -9.81f;
+    //private float jumpVelocity;
     /* // IK
      [SerializeField] private Transform rightToHold;
      [SerializeField] private Transform leftToHold;
@@ -59,8 +75,8 @@ public class Movements : MonoBehaviour
 
     private void GetMoveValues(InputAction.CallbackContext context)
     {
-        playerMove.x = context.ReadValue<Vector2>().x;
-        playerMove.z = context.ReadValue<Vector2>().y;
+        playerMove_.x = context.ReadValue<Vector2>().x;
+        playerMove_.z = context.ReadValue<Vector2>().y;
     }
     private void GetSprintValue(InputAction.CallbackContext context)
     {
@@ -69,6 +85,8 @@ public class Movements : MonoBehaviour
     private void GetJumpValue(InputAction.CallbackContext context)
     {
         playerJump_ = context.ReadValueAsButton();
+        if (context.performed)
+            Jump();
     }
     private void GetCrouchValues(InputAction.CallbackContext context)
     {
@@ -77,6 +95,7 @@ public class Movements : MonoBehaviour
 
     private void Update()
     {
+        //IsGroundedCheck();
         Move();
         Animations();
     }
@@ -84,13 +103,11 @@ public class Movements : MonoBehaviour
     {
         if (stopPlayerMove)
         {
-            playerMove.x = 0;
-            playerMove.z = 0;
+            playerMove_.x = 0;
+            playerMove_.z = 0;
         }
 
         //Debug.Log(playerMove);
-        float applyWalkSpeed;
-        float applySprintSpeed;
 
         /*playerMove.y = inWater ? 0f : playerGravity;// player not floating*/
         if (inWater)
@@ -105,35 +122,46 @@ public class Movements : MonoBehaviour
         applyWalkSpeed = (inWater || playerCrouch_) ? walkSpeed / 2 : walkSpeed;
         applySprintSpeed = (inWater || playerCrouch_) ? sprintSpeed / 2 : sprintSpeed;
 
-        //Debug.Log($"applywalk : {applyWalkSpeed}, applysprint : {applySprintSpeed}");
+        Debug.Log($"applywalk : {applyWalkSpeed}, applysprint : {applySprintSpeed}");
 
-        moveDirection = transform.TransformVector(playerMove);
-        Vector3 movement;
+        moveDirection = transform.TransformVector(playerMove_);
+
         if (playerSprint_)
         {
             movement = applySprintSpeed * Time.deltaTime * moveDirection;
+            //Debug.Log($"sprint {rb.velocity}");
         }
         else
         {
             movement = applyWalkSpeed * Time.deltaTime * moveDirection;
+            //Debug.Log($"walk {rb.velocity}");
         }
-        Vector3 currentPosition = transform.position;
-        Vector3 newPosition = currentPosition + movement;
+        currentPosition = transform.position;
+        newPosition = currentPosition + movement;
         transform.position = newPosition;
 
-        IsGroundedCheck();
+        if (playerCrouch_)
+        {
+            // change height and offset
+            capsuleCollider.height = crouchHeight;
+            capsuleCollider.center = crouchCenter;
+        }
+        else
+        {
+            capsuleCollider.height = standHeight;
+            capsuleCollider.center = standCenter;
+        }
+    }
 
-        if (playerJump_ && isGrounded)
+    private void Jump()
+    {
+        Debug.Log($"Jump!");
+        /*if (playerJump_ && isGrounded)
         {
             isGrounded = false;
             Jump();
-        }
+        }*/
     }
-    void Jump()
-    {
-        rb.AddForce(jumpForce * Time.deltaTime * Vector3.up, ForceMode.Impulse);
-    }
-
     void IsGroundedCheck()
     {
         if (isGrounded)
@@ -153,17 +181,76 @@ public class Movements : MonoBehaviour
             {
                 isGrounded = true;
             }
-            //if (hit.transform.tag == "Ground")
-            //    isGrounded = true;
         }
-        //else
-        //{
-        //    Debug.DrawRay(transform.position, Vector3.down * hit.distance, Color.yellow);
-        //    Debug.Log($"not hiting ground");
-        //    isGrounded = false;
-        //}
+    }
+    private void Animations()
+    {
+        intended = new Vector2(playerMove_.x, playerMove_.z);
+        intended /= (playerSprint_ ? 1f : 2f);
+
+        currentValue = new Vector2(animator.GetFloat("VelocityX"), animator.GetFloat("VelocityZ"));
+
+        result = Vector2.Lerp(currentValue, intended, animationTransitionMultiplier * Time.deltaTime);
+
+        animator.SetFloat("VelocityX", result.x);
+        animator.SetFloat("VelocityZ", result.y);
+
+        if (playerCrouch_ && !inWater)
+        {
+            animator.SetBool("Crouch", true);
+            animator.SetFloat("VelocityX", playerMove_.x);
+            animator.SetFloat("VelocityZ", playerMove_.z);
+        }
+        else
+        {
+            animator.SetBool("Crouch", false);
+        }
+        if (inWater)
+        {
+            animator.SetBool("InWater", true);
+            animator.SetFloat("VelocityX", result.x);
+            animator.SetFloat("VelocityZ", result.y);
+        }
+        else
+        {
+            animator.SetBool("InWater", false);
+        }
+        if (playerJump_)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
     }
 
+    private void LateUpdate()
+    {
+        facingDirection = Vector3.Cross(mainCamera.transform.right, Vector3.up);
+        transform.forward = facingDirection.normalized;
+    }
+    private void OnDisable()
+    {
+        inputActions.Disable();
+        inputActions.Player.Movements.started -= GetMoveValues;
+        inputActions.Player.Movements.performed -= GetMoveValues;
+        inputActions.Player.Movements.canceled -= GetMoveValues;
+
+        inputActions.Player.Sprint.started -= GetSprintValue;
+        inputActions.Player.Sprint.performed -= GetSprintValue;
+        inputActions.Player.Sprint.canceled -= GetSprintValue;
+
+        inputActions.Player.Jump.started -= GetJumpValue;
+        inputActions.Player.Jump.performed -= GetJumpValue;
+        inputActions.Player.Jump.canceled -= GetJumpValue;
+
+        inputActions.Interactions.Crouch.started -= GetCrouchValues;
+        inputActions.Interactions.Crouch.performed -= GetCrouchValues;
+        inputActions.Interactions.Crouch.canceled -= GetCrouchValues;
+    }
+
+    #region needToBeDone
     /*private void OnAnimatorIK()
     {
         if (animator)
@@ -192,70 +279,10 @@ public class Movements : MonoBehaviour
         }
     }*/
 
-    private void Animations()
+    /*void Jump()
     {
-        Vector2 intended = new Vector2(playerMove.x, playerMove.z);
-        intended /= (playerSprint_ ? 1f : 2f);
+        rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+    }*/
 
-        Vector2 currentValue = new Vector2(animator.GetFloat("VelocityX"), animator.GetFloat("VelocityZ"));
-
-        Vector2 result = Vector2.Lerp(currentValue, intended, animationTransitionMultiplier * Time.deltaTime);
-
-        animator.SetFloat("VelocityX", result.x);
-        animator.SetFloat("VelocityZ", result.y);
-
-        if (playerCrouch_ && !inWater)
-        {
-            animator.SetBool("Crouch", true);
-            animator.SetFloat("VelocityX", playerMove.x);
-            animator.SetFloat("VelocityZ", playerMove.z);
-        }
-        else
-        {
-            animator.SetBool("Crouch", false);
-        }
-        if (inWater)
-        {
-            animator.SetBool("InWater", true);
-            animator.SetFloat("VelocityX", result.x);
-            animator.SetFloat("VelocityZ", result.y);
-        }
-        else
-        {
-            animator.SetBool("InWater", false);
-        }
-        if (playerJump_)
-        {
-            animator.SetBool("Jump", true);
-        }
-        else
-        {
-            animator.SetBool("Jump", false);
-        }
-    }
-
-    private void LateUpdate()
-    {
-        Vector3 facingDirection = Vector3.Cross(mainCamera.transform.right, Vector3.up);
-        transform.forward = facingDirection.normalized;
-    }
-    private void OnDisable()
-    {
-        inputActions.Disable();
-        inputActions.Player.Movements.started -= GetMoveValues;
-        inputActions.Player.Movements.performed -= GetMoveValues;
-        inputActions.Player.Movements.canceled -= GetMoveValues;
-
-        inputActions.Player.Sprint.started -= GetSprintValue;
-        inputActions.Player.Sprint.performed -= GetSprintValue;
-        inputActions.Player.Sprint.canceled -= GetSprintValue;
-
-        inputActions.Player.Jump.started -= GetJumpValue;
-        inputActions.Player.Jump.performed -= GetJumpValue;
-        inputActions.Player.Jump.canceled -= GetJumpValue;
-
-        inputActions.Interactions.Crouch.started -= GetCrouchValues;
-        inputActions.Interactions.Crouch.performed -= GetCrouchValues;
-        inputActions.Interactions.Crouch.canceled -= GetCrouchValues;
-    }
+    #endregion
 }
